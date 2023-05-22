68568bdadf9855b4f8837b56bd7fcadeab03affe
Ashwini Raman <ashwini.raman@mav3rik.com>
2023-05-23 09:02:19 +1000

    removed db from next js

--- pages/researchers/index.js
+++ pages/researchers/index.js
@@ -1,8 +1,6 @@
 import Layout from '../../components/layout';
 import styles from './researchers.module.css';
 import { Space, Table, Tag } from 'antd';
-const db = require("../api/db");
-const pgp = require("pg-promise")();
 
 const columns = [
   {
@@ -18,36 +16,36 @@ const columns = [
   },
 ];
 
-// const data = [
-//   {
-//     key: '1',
-//     name: 'Mike',
-//     email: 'mike@test.com',
-//     address: '10 Downing Street',
-//   },
-//   {
-//     key: '2',
-//     name: 'John',
-//     email: 'john@test.com',
-//     address: '10 Downing Street',
-//   },
-// ];
+const data = [
+  {
+    key: '1',
+    name: 'Mike',
+    email: 'mike@test.com',
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    email: 'john@test.com',
+    address: '10 Downing Street',
+  },
+];
 
-export async function getStaticProps() {
-  const query = pgp.as.format(
-    ` SELECT id, name, email
-      FROM clinical_trials.users`,
-  );
-  const result = await db.query(query);
-  return {
-    props: {
-      data: result,
-    },
-    revalidate: 10
-  };
-}
+// export async function getStaticProps() {
+//   const query = pgp.as.format(
+//     ` SELECT id, name, email
+//       FROM clinical_trials.users`,
+//   );
+//   const result = await db.query(query);
+//   return {
+//     props: {
+//       data: result,
+//     },
+//     revalidate: 10
+//   };
+// }
 
-export default function Researchers({data}) {
+export default function Researchers() {
   return (
     <Layout>
       <div className={styles.pageTitle}>
