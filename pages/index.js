e974985d679f3470b69093e3477032148597a484
Ashwini Raman <ashwini.raman@mav3rik.com>
2023-05-23 08:05:54 +1000

    cleaning up

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,11 @@
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
 return (
   <Layout home>
-    {/* Keep the existing code here */}
-
-    {/* Add this <section> tag below the existing <section> tag */}
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <h2 className={utilStyles.headingLg}>Blog</h2>
-      <ul className={utilStyles.list}>
-        {allPostsData.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-          <Link href={`/posts/${id}`}>{title}</Link>
-          <br />
-          <small className={utilStyles.lightText}>
-            {date}
-          </small>
-        </li>
-        ))}
-      </ul>
-    </section>
+    Home page
   </Layout>
 );
 }
